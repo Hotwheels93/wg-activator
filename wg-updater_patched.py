e3a9036672df3da3dd341e5a4459ecb70fb8b3d2
Hotwheels93 <38242626+Hotwheels93@users.noreply.github.com>
2019-05-20 13:22:20 +0200

    Create wg-updater_patched.py
    Patched version of wg-updater.py, updated to work with latest changes on wg-gesucht.de
    
    Changens (line 40-42)
    
    contact = driver.find_element_by_class_name('btn-orange')
        #edit = contact.find_element_by_class_name('btn-orange')
        #edit.send_keys(Keys.TAB)

---
+++ wg-updater_patched.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+import time
+from configparser import ConfigParser
+
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Load configuration
+config = ConfigParser()
+conf_file = open('config.ini')
+config.readfp(conf_file)
+USERNAME = config.get('Login', 'email')
+PASSWORD = config.get('Login', 'password')
+LISTING_URL = config.get('Listing', 'listing_url')
+DELAY = int(config.get('Driver', 'delay'))
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver' # mac-only
+
+# Login
+driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+driver.get('https://wg-gesucht.de')
+# Remove cookie button
+btn = driver.find_element_by_id("cookie-confirm").click()
+driver.find_element_by_link_text("LOGIN").click()
+time.sleep(1)
+username = driver.find_element_by_name("login_email_username")
+password = driver.find_element_by_id("login_password")
+username.send_keys(USERNAME)
+password.send_keys(PASSWORD)
+driver.find_element_by_id('login_submit').click()
+time.sleep(5)
+
+while True:
+    # Open listing
+    driver.get(
+        LISTING_URL)
+    time.sleep(3)
+    contact = driver.find_element_by_class_name('btn-orange')
+    #edit = contact.find_element_by_class_name('btn-orange')
+    #edit.send_keys(Keys.TAB)
+    time.sleep(1)
+    contact.click()
+    time.sleep(3)
+
+    # Refresh listing
+    btn = driver.find_element_by_class_name('btn-orange') # weiter
+    btn.send_keys(Keys.TAB)
+    time.sleep(2)
+    btn.send_keys(Keys.SPACE)
+    btn = driver.find_element_by_class_name('btn-orange') # Änderungen ubernehmen
+    btn.send_keys(Keys.TAB)
+    time.sleep(2)
+    btn.send_keys(Keys.SPACE)
+    assert 'zehn Minuten' in driver.page_source
+    print("Reloaded at {}".format(datetime.now().strftime("%Y-%m-%d %H:%M")))
+    time.sleep(DELAY)
